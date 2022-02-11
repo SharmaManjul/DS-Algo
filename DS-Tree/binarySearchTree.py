6b76f3c7ea2015b47524bece2977218b92f110b4
SharmaManjul <manjul199912@gmail.com>
2022-02-10 19:33:53 -0600

    Added insert

--- DS-Tree/binarySearchTree.py
+++ DS-Tree/binarySearchTree.py
@@ -1,20 +1,38 @@
 class Node:
     def __init__(self, value):
-        self.left = null;
-        self.right = null;
+        self.left = None;
+        self.right = None;
         self.value = value;
 
 class BinarySearchTree:
-    def __init__(self, value):
-        self.root = null;
+    def __init__(self):
+        self.root = None;
 
-    def insert(value):
-        //Code here
+    def insert(self, value):
+        newNode = Node(value)
+        if self.root == None:
+            self.root = newNode
+        else:
+            curNode = self.root
+            while True:
+                if newNode.value < curNode.value:
+                    if curNode.left == None:
+                        curNode.left = newNode
+                        return self
+                    else:
+                        curNode = curNode.left
+                else:
+                    if curNode.right == None:
+                        curNode.right = newNode
+                        return self
+                    else:
+                        curNode = curNode.right
 
-    def lookUp(value):
-        //Code here
-
-        // remove
+def printTree(node, level=0):
+    if node != None:
+        printTree(node.left, level + 1)
+        print(' ' * 4 * level + '->', node.value)
+        printTree(node.right, level + 1)
 
 tree = BinarySearchTree();
 tree.insert(9)
@@ -24,15 +42,4 @@ tree.insert(20)
 tree.insert(170)
 tree.insert(15)
 tree.insert(1)
-JSON.stringify(traverse(tree.root))
-
-//     9
-//  4     20
-//1  6  15  170
-
-function traverse(node) {
-  const tree = { value: node.value };
-  tree.left = node.left === null ? null : traverse(node.left);
-  tree.right = node.right === null ? null : traverse(node.right);
-  return tree;
-}
+printTree(tree.root, 0)
