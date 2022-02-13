b7898197f7134633123bcb5949a1d599555cb97e
SharmaManjul <manjul199912@gmail.com>
2022-02-13 09:37:58 -0600

    Added factorial

---
+++ Algorithms/Recursion/recursiveFactorial.py
@@ -0,0 +1,19 @@
+#Find the factorial using recursion and iteration.
+
+#Recursively
+def recursiveFactorial(value):
+    #Base Case
+    if value == 0:
+        return 1
+    return value * recursiveFactorial(value-1)
+
+print(recursiveFactorial(800))
+
+#Iteratively
+def iterativeFactorial(value):
+    factorial = 1
+    for i in range(1,value+1):
+        factorial *= i
+    return factorial
+
+print(iterativeFactorial(800))
