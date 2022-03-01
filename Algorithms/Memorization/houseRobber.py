a54d9a8e3eff6a579563e97b278f945d66974132
SharmaManjul <manjul199912@gmail.com>
2022-03-01 08:22:21 -0600

    Added house robber with DP and recursion

--- Algorithms/Memorization/houseRobber.py
+++ Algorithms/Memorization/houseRobber.py
@@ -1,4 +1,4 @@
-#Leet code #198: House robber done using dynamic programming without recursoin.
+#Leet code #198: House robber done using dynamic programming without recursion.
 
 def rob(self, nums):
     rob1 ,rob2 = 0, 0
@@ -7,3 +7,17 @@ def rob(self, nums):
         rob1 = rob2
         rob2 = temp
     return rob2
+
+# Done using DP and recursion.
+    def rob(self, nums):
+        rob1 ,rob2, i = 0, 0, -1
+
+        def robbing(rob1, rob2, i, nums):
+            if i == len(nums)-1:
+                return rob2
+            else:
+                print(rob2)
+                i+=1
+            return robbing(rob2, max(nums[i]+rob1, rob2), i, nums)
+
+        return robbing(rob1, rob2, i, nums)
