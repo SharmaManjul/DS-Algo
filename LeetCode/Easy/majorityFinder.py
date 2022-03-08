38902f2e7546f645737be86d67aa67401b94c9d1
SharmaManjul <manjul199912@gmail.com>
2022-03-08 09:18:55 -0600

    Added majority finder using boyer-moore algo.

--- LeetCode/Easy/majorityFinder.py
+++ LeetCode/Easy/majorityFinder.py
@@ -9,3 +9,17 @@ def majorityElement(self, nums):
         if countMap[num] > maxCount:
             maxCount, result = countMap[num], num
     return result
+
+#SC=O(1) solution using Boyer-Moore voting algorithm which returns the majority
+#element given that a an element occures atleast lenght/2 times in the list.
+def majorityElement(self, nums):
+    result, count = 0, 0
+    for num in nums:
+        if count == 0:
+            result = num
+            count += 1
+        elif result == num:
+            count += 1
+        else:
+            count -= 1
+    return result
