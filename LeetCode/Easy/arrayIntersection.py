84ea75513a24f968d428c1bdff64136948d4652b
SharmaManjul <manjul199912@gmail.com>
2022-03-13 15:12:41 -0500

    Added array intersection with two pointer method

--- LeetCode/Easy/arrayIntersection.py
+++ LeetCode/Easy/arrayIntersection.py
@@ -14,3 +14,21 @@ def intersect(self, nums1, nums2):
             res.append(j)
             arrayMap[j] -= 1
     return res
+
+#Using two pointer method with sorting, TC=O(n logn) and SC = O(n)
+def intersect(self, nums1, nums2):
+    nums1.sort()
+    nums2.sort()
+
+    pt1, pt2, res = 0, 0, []
+
+    while pt1 < len(nums1) and pt2 < len(nums2):
+        if nums1[pt1] < nums2[pt2]:
+            pt1 +=1
+        elif nums1[pt1] > nums2[pt2]:
+            pt2 +=1
+        else:
+            res.append(nums1[pt1])
+            pt1 += 1
+            pt2 += 1
+    return res
