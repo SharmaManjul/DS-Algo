cf9f7b442b1a55d583290a2179089f902c00bb67
SharmaManjul <manjul199912@gmail.com>
2022-03-16 20:00:31 -0500

    Added constant time solution for power of 3.

--- LeetCode/Easy/powerOfThree.py
+++ LeetCode/Easy/powerOfThree.py
@@ -11,3 +11,7 @@ def isPowerOfThree(self, n):
             return False
         power += 1
 
+#We know the max integer possible that is a power of 3 is 1162261467 so the modulus with n if n is a power of 3 will always be 0.
+#TC=O(1) and SC=O(1)
+def isPowerOfThree(self, n):
+    return n > 0 and 1162261467 % n == 0
