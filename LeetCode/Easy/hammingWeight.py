8febbc2b49c5eccb2a231ecc67a170f7773b9ef0
SharmaManjul <manjul199912@gmail.com>
2022-03-16 20:19:01 -0500

    Added hamming weight calculator with bit shifting

--- LeetCode/Easy/hammingWeight.py
+++ LeetCode/Easy/hammingWeight.py
@@ -3,3 +3,11 @@
 #bin function reduces integer to binary and the count function counts the 1.
 def hammingWeight(self, n)
     return bin(n).count('1')
+
+#Everytime we AND n and n-1 the result contains one less 1 until it is all zeros. So we can loop and count.
+ def hammingWeight(self, n):
+    counter = 0
+    while n:
+        n &= n-1
+        counter += 1
+    return counter
