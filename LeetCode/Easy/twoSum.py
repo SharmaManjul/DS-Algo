d6f558dfb00824ea8903770613a4388e9772d789
SharmaManjul <manjul199912@gmail.com>
2022-03-19 10:23:37 -0500

    Hash map solution for two sum

--- LeetCode/Easy/twoSum.py
+++ LeetCode/Easy/twoSum.py
@@ -6,3 +6,12 @@ def twoSum(self, nums, target):
         for j in range(len(nums)):
             if nums[i] == target - nums[j] and j != i:
                 return [i, j]
+
+#Using hashmap to store all compliments with TC=O(n) and SC=O(n)
+def twoSum(self, nums, target):
+    numsMap = dict()
+    for i in range(len(nums)):
+        if nums[i] in numsMap:
+            return [i, numsMap[nums[i]]]
+        else:
+            numsMap[target - nums[i]] = i
