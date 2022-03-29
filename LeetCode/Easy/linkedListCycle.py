5f583f1c371714f72cbb24ad82c0b27d5c4a9e28
SharmaManjul <manjul199912@gmail.com>
2022-03-29 08:38:08 -0500

    Added LL cycle checker.

---
+++ LeetCode/Easy/linkedListCycle.py
@@ -0,0 +1,12 @@
+#Check if linked list has a cycle in it.
+
+#Using while loop and a hash set with TC = O(n) and SC = O(n)
+def hasCycle(self, head):
+    hashSet = {}
+    while head != None:
+        if head in hashSet:
+            return True
+        else:
+            hashSet[head] = True
+            head = head.next
+    return False
