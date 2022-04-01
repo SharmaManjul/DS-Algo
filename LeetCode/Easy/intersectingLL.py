90dedc1af35e1eea7c0ecb3f7e1ed685e650120c
SharmaManjul <manjul199912@gmail.com>
2022-04-01 08:19:34 -0500

    Added optimized solution for intersecting LL

--- LeetCode/Easy/intersectingLL.py
+++ LeetCode/Easy/intersectingLL.py
@@ -10,3 +10,20 @@ def getIntersectionNode(self, headA, headB):
         if headB in mapA:
             return headB
         headB = headB.next
+
+#Using two pointers that switch heads when None is encountered this brings them to the same level and offsets the length
+#difference.
+#TC=O(n+m) and SCO(1)
+def getIntersectionNode(self, headA, headB):
+    pA = headA
+    pB = headB
+    while pA != pB:
+        if pA == None:
+            pA = headB
+        else:
+            pA = pA.next
+        if pB == None:
+            pB = headA
+        else:
+            pB = pB.next
+    return pA
