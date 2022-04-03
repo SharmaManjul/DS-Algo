711f13b9fb3b96964c148eb15bbb2fe88d12d10e
SharmaManjul <manjul199912@gmail.com>
2022-04-03 17:25:27 -0500

    Adding optimized rotate array with SC=O(1).

--- LeetCode/Medium/rotateArray.py
+++ LeetCode/Medium/rotateArray.py
@@ -9,3 +9,19 @@ def rotate(self, nums, k):
         for j in range(len(nums)):
             # print(j, (j+k)%len(nums))
             nums[(j + k) % len(nums)] = s_nums[j]
+
+#TC=O(n) and SC=O(1)
+def rotate(self, nums, k):
+    def reversor(nums, l, r):
+        while l < r:
+            temp = nums[l]
+            nums[l] = nums[r]
+            nums[r] = temp
+            r -= 1
+            l += 1
+
+    if not len(nums) == 0 or not k == 0:
+        k = k % len(nums)
+        reversor(nums, 0, len(nums) - k - 1)
+        reversor(nums, len(nums) - k, len(nums) - 1)
+        reversor(nums, 0, len(nums) - 1)
