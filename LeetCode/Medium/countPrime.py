59fb4940f97a31384e1eeb280ca2ef07c41edfe5
SharmaManjul <manjul199912@gmail.com>
2022-04-05 08:42:25 -0500

    Adding SOE algo solution for prime count.

--- LeetCode/Medium/countPrime.py
+++ LeetCode/Medium/countPrime.py
@@ -1,27 +1,26 @@
 #Count all the prime numbers until n
 
 #Using sieve of eratosthenes algorithm which assumes all numbers are prime and then checks if they are prime or not.
-#close to TC=O(n^2) and SC=O(n)
-def countPrimes(self, n):
-    if n < 2:
-        return 0
+#TC=O(n log(log(n))) and SC=O(n)
+    def countPrimes(self, n):
+        if n < 2:
+            return 0
+        count = 0
+        primes = []
+        for p in range(n):
+            primes.append(True)
+        primes[0] = False
+        primes[1] = False
 
-    p_list = []
-    count = 0
-    for i in range(0, n):
-        p_list.append(True)
-
-    i = 2
-    while i * i < n:
-        if p_list[i] == True:
+        for i in range(2, n):
             j = 2
-            while j * i < n:
-                p_list[j * i] = False
-                j += 1
-        i += 1
+            if primes[i] == True:
+                while j * i < n:
+                    primes[j * i] = False
+                    j += 1
 
-    for p in p_list:
-        if p == True:
-            count += 1
+        for prime in primes:
+            if prime == True:
+                count += 1
 
-    return count - 2
+        return count
