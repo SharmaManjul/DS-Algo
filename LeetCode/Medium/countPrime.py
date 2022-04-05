af7c95306d6b20e6dfa9da7812e230b6aa34ea9a
SharmaManjul <manjul199912@gmail.com>
2022-04-05 08:51:24 -0500

    Adding optimized count prime solution.

--- LeetCode/Medium/countPrime.py
+++ LeetCode/Medium/countPrime.py
@@ -24,3 +24,27 @@
                 count += 1
 
         return count
+
+#Only looping till sqrt(n) since factors exist in pairs and if an element hasn't been crossed off until srt(n) then it's prime
+#TC=O(sqrt(n) log(log(n))) and SC=O(n)
+def countPrimes(self, n):
+    if n < 2:
+        return 0
+    count = 0
+    primes = []
+    for p in range(n):
+        primes.append(True)
+    primes[0] = False
+    primes[1] = False
+
+    for i in range(2, int(n ** 0.5) + 1):
+        j = 2
+        if primes[i] == True:
+            while j * i < n:
+                primes[j * i] = False
+                j += 1
+    for prime in primes:
+        if prime == True:
+            count += 1
+
+    return count
