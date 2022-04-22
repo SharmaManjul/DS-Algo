afa2c120b6c1eb901c7d58882e2a61f15fb77d96
SharmaManjul <manjul199912@gmail.com>
2022-04-21 19:44:07 -0500

    Adding optimized circular array.

--- LeetCode/Hard/grok_circularArray.py
+++ LeetCode/Hard/grok_circularArray.py
@@ -35,3 +35,48 @@ def circularArrayLoop(self, nums):
 
 #If you maintain a hash map of all the number that don't result in cycles then we cna reduce the TC to O(N) as we would
 # only need to loop once through the array but that would mean our SC is now O(N).
+class Solution(object):
+
+    def get_next_item(self, prev_direction, nums, cur_index, seen):
+        # Checking if there is change in direction.
+
+        cur_direction = nums[cur_index] >= 0
+        if cur_direction != prev_direction:
+            seen[cur_index] = True
+            return -1
+
+        next_index = (cur_index + nums[cur_index]) % len(nums)
+
+        # Chekcing if there is single element cycle.
+        if next_index == cur_index:
+            seen[cur_index] = True
+            return -1
+
+        return next_index
+
+    def circularArrayLoop(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        visited = {}
+        for i in range(len(nums)):
+            # True is forward and False is backward.
+            cur_direction = nums[i] >= 0
+            slow, fast = i, i
+
+            if i in visited:
+                continue
+
+            while True:
+                slow = self.get_next_item(cur_direction, nums, slow, visited)
+                fast = self.get_next_item(cur_direction, nums, fast, visited)
+                if fast != -1:
+                    fast = self.get_next_item(cur_direction, nums, fast, visited)
+
+                if slow == -1 or fast == -1 or slow == fast:
+                    break
+
+            if slow != -1 and slow == fast:
+                return True
+        return False
