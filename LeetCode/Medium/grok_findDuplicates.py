23d51ff9694961ad71471e3f20fc782d063f6be3
SharmaManjul <manjul199912@gmail.com>
2022-04-25 19:06:41 -0500

    Adding more optimal solu to finding duplicates in 1 to n.

--- LeetCode/Medium/grok_findDuplicates.py
+++ LeetCode/Medium/grok_findDuplicates.py
@@ -13,3 +13,16 @@ def findDuplicate(self, nums):
     for i in range(len(nums)):
         if nums[i] != i + 1:
             return nums[i]
+
+#More readable and slightly more efficient solution.
+    def findDuplicate(self, nums):
+        i = 0
+        while i < len(nums):
+            if nums[i] != i+1:
+                j = nums[i] - 1
+                if nums[i] != nums[j]:
+                    nums[i], nums[j] = nums[j], nums[i]
+                else:
+                    return nums[i]
+            else:
+                i += 1
