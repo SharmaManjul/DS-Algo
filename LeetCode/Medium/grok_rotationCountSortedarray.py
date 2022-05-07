197f8ac66a19ce3e4cd589d0fe93d7eac3bbe53f
manjul <manjul199912@gmail.com>
2022-05-07 12:22:32 -0500

    Adding rotation count of sorted array with duplication.

--- LeetCode/Medium/grok_rotationCountSortedarray.py
+++ LeetCode/Medium/grok_rotationCountSortedarray.py
@@ -29,3 +29,56 @@ def main():
 
 
 main()
+
+# More readable solution:
+def findMin(self, arr: List[int]) -> int:
+    start, end = 0, len(arr) - 1
+
+    # Fnd the largets element in the list i.e. the last element of the pre rotated array.
+    while start < end:
+        mid = start + (end - start) // 2
+        # Check if mid within bounds to prevent calculating mid+1 or mid-1 and if all good return minimum
+        if mid < end and arr[mid] > arr[mid + 1]:
+            return arr[mid + 1]
+        if mid > start and arr[mid] < arr[mid - 1]:
+            return arr[mid]
+
+        if arr[start] < arr[mid]:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    # If not returns in the while that means list is not rotated.
+    return arr[0]
+
+
+# Rotation with duplication.
+
+    def findMin(self, arr: List[int]) -> int:
+        start, end = 0, len(arr) - 1
+
+        while start < end:
+            mid = start + (end - start) // 2
+
+            # Check if mid within bounds to prevent calculating mid+1 or mid-1 and if all good return minimum
+            if mid < end and arr[mid] > arr[mid + 1]:
+                return arr[mid + 1]
+            if mid > start and arr[mid] < arr[mid - 1]:
+                return arr[mid]
+
+            # If start, mid and end are equal we need to shrink the window size but also check for any condition violations.
+            if arr[start] == arr[mid] and arr[end] == arr[mid]:
+                if arr[start] > arr[start + 1]:
+                    return arr[start + 1]
+                start += 1
+
+                if arr[end] < arr[end - 1]:
+                    return arr[end]
+                end -= 1
+
+            elif arr[start] <= arr[mid]:
+                start = mid + 1
+            else:
+                end = mid - 1
+
+        return arr[0]
