48fa53dac0fe520702abdc7e099140a0414249ed
manjul <manjul199912@gmail.com>
2022-05-14 17:20:42 -0500

    Adding count subset sums clean code.

--- LeetCode/Hard/grok_countSubsetSums.py
+++ LeetCode/Hard/grok_countSubsetSums.py
@@ -26,26 +26,54 @@
 #
 # main()
 
+
+
+# # Iteratively using bottom up and tabulation
+# # TC = O(N * S) and SC = (S)
+# def count_subsets(num, sum):
+#     n = len(num)
+#     dp = [[-1 for x in range(sum+1)] for y in range(2)]
+#     # populate the sum = 0 columns, as we will always have an empty set for zero sum
+#     for i in range(0, 2):
+#         dp[i][0] = 1
+#     # with only one number, we can form a subset only when the required sum is
+#     # equal to its value
+#     for s in range(1, sum+1):
+#         dp[0][s] = 1 if num[0] == s else 0
+#     # process all subsets for all sums
+#     for i in range(1, n):
+#         for s in range(1, sum+1):
+#             # exclude the number
+#             dp[i%2][s] = dp[(i - 1)%2][s]
+#             # include the number, if it does not exceed the sum
+#             if s >= num[i]:
+#                 dp[i%2][s] += dp[(i - 1)%2][s - num[i]]
+#     # the bottom-right corner will have our answer.
+#     return dp[(n-1)%2][sum]
+#
+# def main():
+#     print("Total number of subsets " + str(count_subsets([1, 1, 2, 3], 4)))
+#     print("Total number of subsets: " + str(count_subsets([1, 2, 7, 1, 5], 9)))
+#
+# main()
+
+
+# Cleaner approach using 1-d list
 def count_subsets(num, sum):
     n = len(num)
-    dp = [[-1 for x in range(sum+1)] for y in range(2)]
-    # populate the sum = 0 columns, as we will always have an empty set for zero sum
-    for i in range(0, 2):
-        dp[i][0] = 1
+    dp = [0 for x in range(sum+1)]
+    dp[0] = 1
     # with only one number, we can form a subset only when the required sum is
     # equal to its value
     for s in range(1, sum+1):
-        dp[0][s] = 1 if num[0] == s else 0
+        dp[s] = 1 if num[0] == s else 0
     # process all subsets for all sums
     for i in range(1, n):
-        for s in range(1, sum+1):
-            # exclude the number
-            dp[i%2][s] = dp[(i - 1)%2][s]
-            # include the number, if it does not exceed the sum
-            if s >= num[i]:
-                dp[i%2][s] += dp[(i - 1)%2][s - num[i]]
-    # the bottom-right corner will have our answer.
-    return dp[(n-1)%2][sum]
+        for s in range(sum, -1, -1):
+            if num[i] <= s:
+                dp[s] += dp[s - num[i]]
+
+    return dp[sum]
 
 def main():
     print("Total number of subsets " + str(count_subsets([1, 1, 2, 3], 4)))
