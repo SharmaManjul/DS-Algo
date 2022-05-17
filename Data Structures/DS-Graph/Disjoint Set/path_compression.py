42201adb3affe84c827d2fbe7fed756697c9d8c7
manjul <manjul199912@gmail.com>
2022-05-17 18:57:52 -0500

    Adding disjoint set using union by path compression.

---
+++ Data Structures/DS-Graph/Disjoint Set/path_compression.py
@@ -0,0 +1,37 @@
+# UnionFind class
+# TC best case: O(1) and worst case: O(N) but only once so average TC is O(logN)
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+
+    def find(self, x):
+        if x == self.root[x]:
+            return self.root[x]
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x != root_y:
+            self.root[x] = root_y
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
+
+# Test Case
+uf = UnionFind(10)
+# 1-2-5-6-7 3-8-9 4
+uf.union(1, 2)
+uf.union(2, 5)
+uf.union(5, 6)
+uf.union(6, 7)
+uf.union(3, 8)
+uf.union(8, 9)
+print(uf.connected(1, 5))  # true
+print(uf.connected(5, 7))  # true
+print(uf.connected(4, 9))  # false
+# 1-2-5-6-7 3-8-9-4
+uf.union(9, 4)
+print(uf.connected(4, 9))  # true
