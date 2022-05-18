187aaf8dda38fdf687e7dc22fed083666bd7a10d
manjul <manjul199912@gmail.com>
2022-05-17 19:17:25 -0500

    Adding disjoint set using union by rank and path compression.

---
+++ Data Structures/DS-Graph/Disjoint Set/union_by_rank_with_path_compression.py
@@ -0,0 +1,50 @@
+# UnionFind class
+# If we look at what find and union are doing we see that they are helping each other become more efficient. The find is
+# compressing the parent values to the root they point to and the union is splitting them out into smaller trees. So if
+# we think on an average case the TC will actually be O(1) and more explanation becauase of the ackermann function
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        # Use a rank array to record the height of each vertex, i.e., the "rank" of each vertex.
+        # The initial "rank" of each vertex is 1, because each of them is
+        # a standalone vertex with no connection to other vertices.
+        self.rank = [1] * size
+
+    # The find function here is the same as that in the disjoint set with path compression.
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    # The union function with union by rank
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if self.rank[rootX] > self.rank[rootY]:
+            self.root[y] = rootX
+        elif self.rank[rootY] > self.rank[rootX]:
+            self.root[x] = rootY
+        else:
+            self.root[x] = rootY
+            self.rank[rootY] += 1
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
+
+# Test Case
+uf = UnionFind(10)
+# 1-2-5-6-7 3-8-9 4
+uf.union(1, 2)
+uf.union(2, 5)
+uf.union(5, 6)
+uf.union(6, 7)
+uf.union(3, 8)
+uf.union(8, 9)
+print(uf.connected(1, 5))  # true
+print(uf.connected(5, 7))  # true
+print(uf.connected(4, 9))  # false
+# 1-2-5-6-7 3-8-9-4
+uf.union(9, 4)
+print(uf.connected(4, 9))  # true
